f3b0eabeb462868ddc07f472e646a83d1995bcd4
igorkll <69525332+igorkll@users.noreply.github.com>
2022-07-28 12:43:28 +0300

    Update forOpenOS.lua

--- forOpenOS.lua
+++ forOpenOS.lua
@@ -1,7 +1,16 @@
 local shell = require("shell")
 local fs = require("filesystem")
+local serialization = require("serialization")
+local component = require("component")
 
 local args, options = shell.parse(...)
+
+if #args == 0 then
+    print("eeprom flash <.eeprom file>; flash .eeprom file to eeprom chip")
+    print("eeprom dump")
+    return
+end
+
 local path = shell.resolve(args[1])
 if not path or not fs.exists(path) then
     io.stderr:write("file not found\n")
@@ -11,3 +20,15 @@ end
 
 ------------------------------------
 
+local function findEeprom()
+    print("finding eeprom")
+    if component.isAvailable("eeprom") then
+        print("finded eeprom " .. component.eeprom.address)
+        return component.eeprom
+    end
+    print("eeprom is not found")
+    print("exit.")
+    os.exit()
+end
+
+------------------------------------
